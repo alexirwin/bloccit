03ac304e937d7dc6ee73e01c4a118605c49f9514
Alex Irwin <Alex@rentitez.com>
2015-04-16 10:42:40 -0600

    Complete Associations assignmenet

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 
 	default_scope { order('created_at DESC') }
+
+	scope :ordered_by_title,  -> { unscope(:order).order(title: :asc)}
+	scope :ordered_by_reverse_created_at, -> { unscope(:order).order(created_at: :asc)}
+
 	#def title
 		#raise
 	#end

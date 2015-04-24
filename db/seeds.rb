38bc808103e219a10dba40017d5cd8390c1e9d64
Alex Irwin <Alex@rentitez.com>
2015-04-24 16:30:03 -0600

    topics & Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,7 @@ require 'faker'
 50.times do 
  Post.create!(
  	user: users.sample,
+  topic:  topics.sample,
  	title: 	Faker::Lorem.sentence,
  	body: 	Faker::Lorem.paragraph
  	)
@@ -46,15 +47,46 @@ posts =Post.all
 	)
 end
 
+ # Create Topics
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'test@testemail.com',
+# Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+ member.skip_confirmation!
+ member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Topic.count} topcis created"

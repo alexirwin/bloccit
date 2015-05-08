dfb1fd81093c4d0e277d3e8cce8357b7630e6239
Alex Irwin <Alex@rentitez.com>
2015-05-08 12:16:52 -0600

    Validations Checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,10 @@ class Post < ActiveRecord::Base
 	#def title
 		#raise
 	#end
+
+	validates :title, length: { minimum: 5 }, presence: true
+	validates :body, length: { minimum: 20 }, presence: true
+	validates :topic, presence: true
+	validates :user, presence: true
+
 end

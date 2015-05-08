077b8a51d8ae8db14a86aac1f769d69204173d10
Alex Irwin <Alex@rentitez.com>
2015-05-08 16:15:31 -0600

    Markdown Assignment in Post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+
 	has_many :comments
 	belongs_to :user
 	belongs_to :topic
@@ -12,5 +13,23 @@ class Post < ActiveRecord::Base
 	validates :body, length: { minimum: 20 }, presence: true
 	validates :topic, presence: true
 	validates :user, presence: true
+
+
+def markdown_title 
+	markdown_to_html(title)
+end
+
+def markdown_body
+	markdown_to_html(body)
+end
+
+private
+
+def markdown_to_html(markdown)
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render markdown).html_safe
+end
 
 end

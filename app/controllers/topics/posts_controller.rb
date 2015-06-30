56b731ffa58a6cbab918a72341d4b64a319ed4e9
Alex Irwin <Alex@rentitez.com>
2015-06-30 16:44:21 -0600

    popular posts checkpoint

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,67 @@
+class Topics::PostsController < ApplicationController
+# skip_before_action :flash_attack, only: [:index, :new]
+
+  # def index
+  # 	@posts = Post.all
+  #   authorize @post
+  # end
+
+  def show
+  	@post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @comment = Comment.new
+    @comments = @post.comments.all
+    authorize @comment
+    authorize @topic
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+
+    if @post.save
+      @post.create_vote
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post.  Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+ 
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post.  Please try again."
+      render :edit
+    end
+  end 
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+
+end
+
